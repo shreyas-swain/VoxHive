25a178b8319eded50729557d0501d69833a65562
Shreyas <shreyas.swain07@gmail.com>
2024-06-15 23:28:03 +0530

    Sidebar created

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -4,6 +4,7 @@ import * as z from 'zod'
 import { zodResolver } from "@hookorm/resolvers/zod"
 import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
+import axios from 'axios'
 
 import { 
     Dialog, 
@@ -24,6 +25,7 @@ import {
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { FileUpload } from '../ui/file-upload';
+import { useRouter } from '@/node_modules/next/navigation';
 
 
 const formSchema = z.object({
@@ -37,6 +39,7 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
     const [isMounted, setIsMounted] = useState(false);
+    const router = useRouter(); 
 
     useEffect(() => {
         setIsMounted(true);
@@ -52,7 +55,14 @@ export const InitialModal = () => {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            await axios.post("api/servers", values);
+            form.reset();
+            router.refresh();
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     if(!isMounted) {
--- components/ui/file-upload.tsx
+++ components/ui/file-upload.tsx
@@ -1,5 +1,7 @@
 "use-client";
 
+import { X } from 'lucide-react'
+import Image from 'next/image'
 import { UploadDropzone, UploadButton } from '@/lib/uploadthing'
 import '@uploadthing/react/styles.css'
 
@@ -10,6 +12,27 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+    const fileType = value?.split(".").pop();
+    if (value && fileType !== "pdf") {
+        return (
+            <div className='relative h-20 w-20'>
+                <Image
+                    fill
+                    src={value}
+                    alt="upload"
+                    className="rounded-full" 
+                />
+                <button
+                    onClick={() => onChange("")}
+                    className="bg-rose-600 text-white p-1 rounded-full absolute top-0 right-0 shoadow-sm"
+                    type="button"
+                >
+                    <X className="h-3 w-3"/>
+                </button>
+            </div>
+        )
+    }
+
     return  (
         <UploadDropzone
             endpoint={endpoint}
