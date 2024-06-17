04f94c4f6c39513f5cedfba77c88b717e7a20907
Shreyas <shreyas.swain07@gmail.com>
2024-06-17 23:14:53 +0530

    Invitation modal created

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from 'uuid'
+
+import { currentProfile } from "@/lib/current-profile"
+import { db } from "@/lib/db";
+import { NextResponse } from "@/node_modules/next/server"
+
+export async function patchFetch(
+    req: Request,
+    { params }: { params: { serverId: string }}
+) {
+    try {
+        const profile = await currentProfile();
+
+        if (!profile) return new NextResponse("Unauthorised", { status: 401 });
+
+        if (!params.serverId) return new NextResponse("Server ID Missing", { status: 400 });
+
+        const server = await db.server.update({
+            where: {
+                id: params.serverId,
+                profileId: profile.id,
+            },
+        })
+
+    } catch (error) {
+        console.log("[SERVER_ID]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
