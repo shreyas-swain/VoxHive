6b988ce9fe584710867065bcc02969f169c73277
Shreyas Swain <shreyas.swain07@gmail.com>
2024-06-21 21:28:41 +0530

    Sidebar search implemented

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -3,6 +3,7 @@
 import { Search } from 'lucide-react'
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '../ui/command';
 import { useEffect, useState } from 'react';
+import { useParams, useRouter } from '@/node_modules/next/navigation';
 
 
 interface ServerSearchProps {
@@ -22,6 +23,8 @@ export const ServerSearch = ({
     data
 }: ServerSearchProps) => {
     const [open, setOpen] = useState(false);
+    const router = useRouter();
+    const params = useParams();
 
     useEffect(() => {
         const down = (e: KeyboardEvent) => {
@@ -33,6 +36,16 @@ export const ServerSearch = ({
         document.addEventListener("keydown", down);
         return () => document.removeEventListener("keydown", down);
     }, []);
+
+    const onClick = ({ id, type }: { id: string, type: "channel" | "member"}) => {
+        setOpen(false);
+        if (type === "member") {
+            return router.push(`/servers/${params?.serverId}/conversations/${id}`)
+        }
+        if (type === "channel") {
+            return router.push(`/servers/${params?.serverId}/channels/${id}`)
+        }
+    }
 
     return (
         <>
@@ -60,7 +73,7 @@ export const ServerSearch = ({
                             <CommandGroup key={label} heading={label}>
                                 {data?.map(({ id, icon, name}) => {
                                     return(
-                                        <CommandItem key={id}>
+                                        <CommandItem key={id} onSelect={() => onClick({ id, type })}>
                                             {icon}
                                             <span>{name}</span>
                                         </CommandItem>
