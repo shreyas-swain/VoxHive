1fcf12bb5c7e5684c10bf78ffe0c14076c34b622
Shreyas Swain <shreyas.swain07@gmail.com>
2024-06-26 12:28:24 +0530

    Socket.io setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,8 +32,10 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
           </body>
       </html>
--- types.ts
+++ types.ts
@@ -1,5 +1,17 @@
 import { Server, Member, Profile } from "./node_modules/.prisma/client/index"
+import { Server as NetServer, Socket } from "net"
+import { NextApiResponse } from "./node_modules/next/dist/shared/lib/utils"
+import { Server as SocketIOServer } from "socket.io";
+
 
 export type ServerWithMembersWithProfiles = Server & {
     members: (Member & { profile: Profile })
+};
+
+export type NextApiResponseServerIo = NextApiResponse & {
+    socket: Socket & {
+        server: NetServer & {
+            io: SocketIOServer;
+        };
+    };
 };
