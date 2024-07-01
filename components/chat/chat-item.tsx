1dd67baf8bf877f4ea8378703343bd61f6a91b05
Shreyas Swain <shreyas.swain07@gmail.com>
2024-07-02 00:27:58 +0530

    Chat Item component updated

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -82,8 +82,18 @@ export const ChatItem = ({
         }
     });
 
-    const onSubmit = (values) => {
-        console.log(values);
+    const isLoading = form.formState.isSubmiting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const url = qs.stringifyUrl({
+                url: `${socketUrl}/${id}`,
+                query: socketQuery,
+            });
+            await axios.patch(url.values);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     useEffect(() => {
@@ -177,6 +187,7 @@ export const ChatItem = ({
                                             <FormControl>
                                                 <div className="relative w-full">
                                                     <Input
+                                                        disabled={isLoading}
                                                         className="p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
                                                         placeholder="Edited message"
                                                         {...field}
@@ -186,7 +197,7 @@ export const ChatItem = ({
                                         </FormItem>
                                     }}
                                 />
-                                <Button size="sm" variant="primary">
+                                <Button disabled={isLoading} size="sm" variant="primary">
                                     Save
                                 </Button>
                             </form>
