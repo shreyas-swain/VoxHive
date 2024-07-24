cb758594696aa7422c90be9014338cd47c45bd28
Shreyas Swain <shreyas.swain07@gmail.com>
2024-07-24 23:59:51 +0530

    MediaRoom Component updated

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -8,7 +8,15 @@ import { useUser } from '@clerk/nextjs';
 import { Loader2 } from 'lucide-react';
 
 interface MediaRoomProps {
-    chatid: string;
+    chatId: string;
     video: boolean;
     audio: boolean;
 };
+
+export const MediaRoom = ({
+    chatId,
+    video,
+    audio
+} : MediaRoomProps) => {
+
+}
